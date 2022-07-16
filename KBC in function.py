8c6f14724852697555f05e95559b618031b302dc
Chuningliu <chuningliukahmei@gmail.com>
2022-07-16 11:06:46 +0530

    first commit

--- KBC in function.py
+++ KBC in function.py
@@ -1,12 +1,12 @@
 question_list=["How many continents are there?", "What is capital of India?", "Which course is taught in navgurukul?"]
-option_list=[["1.Four", "2.Nine", "3.Seven", "4.Eight"],["1.Chandigarh", "2.Bhopal", "3.Chennai", "4.Delhi"],["1.Software Engineering", "2.Counseling", "3.Tourism", "4.Agriculture"]]
+option_list=[["Four", "Nine", "Seven", "Eight"],["Chandigarh", "Bhopal", "Chennai", "Delhi"],["Software Engineering", "Counseling", "Tourism", "Agriculture"]]
 answer_list=[3,4,1]
 def option(index):
     j=0
     while j<len(option_list[index]):
         print(j+1,option_list[index][j])
         j=j+1
-    user_ans = int(input('.....'))
+    user_ans = int(input('Enter your choice:- '))
     if user_ans==answer_list[index]:
         return True
     else:
@@ -19,9 +19,9 @@ def que():
         if result=="no":
             index+=1
         elif result==True:
-            print("congratulations")
+            print("Congratulations")
         else:
-            print("you loose")
+            print("You lost")
             break   
         index+=1
 
